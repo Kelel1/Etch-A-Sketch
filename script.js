67e34f84121d923e5c07336945c37ac1057c499f
Kern Elder <kelel1@yahoo.com>
2018-08-01 07:18:23 -0400

    Change container dimensions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('#grid-container');
 
-for(i = 0; i <= 15; i++) {
+for(i = 0; i < 256; i++) {
     const content = document.createElement('div');
     content.classList.add('content'); 
-    content.className = i;   
+    content.className = 'grid-block';       
     container.appendChild(content);
 }
