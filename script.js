e22dc0f3788221eeb8e1779f5916cc2fab4c9110
Kern Elder <kelel1@yahoo.com>
2018-08-01 17:50:16 -0400

    Complete sketch function

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ for(i = 0; i < 256; i++) {
     content.className = 'grid-block';       
     container.appendChild(content);
 }
+
+
+/** Add hover event listener to each block in grid */
+const block = document.getElementsByClassName('grid-block');
+
+for (i = 0; i < block.length; i++) {   
+
+    block[i].addEventListener('mouseover', (play) => {
+
+        play.target.style.backgroundColor = "black";
+
+    })
+}
+
