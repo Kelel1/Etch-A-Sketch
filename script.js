f339a02a3e759d2ae112b8a1d2f36ba6d21479cf
Kern Elder <kelel1@yahoo.com>
2018-08-05 18:05:26 -0400

    Complete main functionality

--- script.js
+++ script.js
@@ -1,51 +1,88 @@
 const container = document.querySelector('#grid-container');
 const TOTAL_PIXELS = 620;
+let numbSquares;
 
-for (i = 0; i < 1600; i++) {
-    const content = document.createElement('div');
-    content.classList.add('content'); 
-    content.className = 'grid-block';       
-    container.appendChild(content);
+
+/** set height/width dimesions of blocks to add to webpage */
+function setDivs(totalSquares) {
+    for (i = 0; i < totalSquares*totalSquares; i++) {
+        const content = document.createElement('div');        
+        content.setAttribute("style", "width: " + TOTAL_PIXELS / totalSquares + "px"+ "; "+
+        "height: " + TOTAL_PIXELS / totalSquares + "px");
+        content.classList.add('content'); 
+        content.className = 'grid-block';          
+        container.appendChild(content);
+        
+    }
 }
 
-
-
-var i = 0;
-var value = " ";
-while (i < 40) {
-    //var value = " ";
-    value += "auto " 
-    // container.style.gridTemplateColumns = value;
-    i++;
-    // console.log(value);
+/** Create an x by x grid */
+function createGrid(totalSquares) {
+    var i = 0;
+    var value = " ";
+    while (i < totalSquares) {        
+        value += "auto "         
+        i++;        
+    }
+    container.style.gridTemplateColumns = value;  
+    
 }
-container.style.gridTemplateColumns = value;
-//console.log(value);
 
 
 
 /** Add hover event listener to each block in grid */
 const block = document.getElementsByClassName('grid-block');
 
-for (i = 0; i < block.length; i++) {   
+function setEventListener() {
 
-    block[i].addEventListener('mouseover', (play) => {
+    for (i = 0; i < block.length; i++) {   
 
-        play.target.style.backgroundColor = "blue";
+        block[i].addEventListener('mouseover', (play) => {
 
-    });
-};
+            play.target.style.backgroundColor = "blue";
 
+        });
+    };
+}
 /** Reset display */
 const reset = document.getElementById('reset');   
+    
+    reset.addEventListener('click', (toReset) => {
+        var div = document.getElementById('grid-container');
+        while(div.firstChild){
+            div.removeChild(div.firstChild);
+        }
 
-reset.addEventListener('click', (toReset) => {
-    for (i = 0; i < block.length; i++) { 
+        for (i = 0; i < block.length; i++) { 
 
-        block[i].style.backgroundColor = "lightblue";
-    }
+            block[i].style.backgroundColor = "lightblue";
+        }
 
-});
+        let total = setTotalSquares();
+        
+        setDivs(total);
+        createGrid(total);
+        setEventListener();      
+
+    });
+
 
 /** Adjust number of squares */
+function setTotalSquares() {
+    let amount = Number(prompt("Enter number of squares"));
+    return amount;    
+}
+
+/** Creat initial game state */
+function start() {
+    numbSquares = 40;
+    
+    setDivs(numbSquares);
+    createGrid(numbSquares);
+    setEventListener(); 
+}
+start();
+
+
+
 
